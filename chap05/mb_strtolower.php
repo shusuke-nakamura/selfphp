341fcf0b8ea78eb4ea593a6858710d918684ddf8
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-09-18 17:15:45 +0900

    P.150まで学習完了

---
+++ chap05/mb_strtolower.php
@@ -0,0 +1,4 @@
+<?php
+mb_internal_encoding('UTF-8');
+print strtolower('ＷＩＮＧＳプロジェクト');
+print mb_strtolower('ＷＩＮＧＳプロジェクト');
---
+++ chap05/strtolower.php
@@ -0,0 +1,6 @@
+<?php
+print strtolower('WINGSプロジェクト');
+print strtoupper('wingsプロジェクト');
+print lcfirst('WINGS PROJECT');
+print ucfirst('wings project');
+print ucwords('wings project');
