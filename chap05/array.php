21b2b21802e8f2d3f1857b2910c716111b64a20f
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-09-19 08:31:02 +0900

    P.174まで学習完了

---
+++ chap05/array.php
@@ -0,0 +1,10 @@
+<?php
+$data = ['高江', '青木', '片淵'];
+print array_push($data, '山田', '土井');
+print_r($data);
+print array_pop($data);
+print_r($data);
+print array_shift($data);
+print_r($data);
+print array_unshift($data, '掛谷', '横塚', '上垣');
+print_r($data);
