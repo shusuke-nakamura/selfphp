193ec5d99e05939caab98c8b7824f8714899b7d1
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-09-21 14:27:24 +0900

    P.322まで学習完了

---
+++ chap08/link1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>クエリ情報</title>
+</head>
+<body>
+  <a href="link2.php?keyword=<?= urlencode('クエリ情報(&%)') ?>">結果を確認</a>
+</body>
+</html>
---
+++ chap08/post2.php
@@ -0,0 +1,11 @@
+<?php require_once '../Encode.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>ポストデータ</title>
+</head>
+<body>
+  こんにちは、<?= e($_POST['name']) ?>さん！
+</body>
+</html>
