902a5283b7a7d960f11962404d2cc5d01bf3f9cd
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-09-22 21:01:52 +0900

    P.409まで学習完了

---
+++ chap09/bindValue_form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>画像のアップロード</title>
+</head>
+<body>
+  <form action="bindValue_process.php" method="post" enctype="multipart/form-data">
+    <label for="photo">アップロードする画像：</label>
+    <input type="file" name="photo" id="photo" size="50" />
+    <input type="submit" value="アップロード" />
+  </form>
+</body>
+</html>
---
+++ chap09/bindValue_process.php
@@ -0,0 +1,14 @@
+<?php
+require_once '../DbManager.php';
+
+try {
+  $db = getDb();
+  $stt = $db->prepare('INSERT INTO photo(type, data) VALUES (:type, :data)');
+  $file = fopen($_FILES['photo']['tmp_name'], 'rb');
+  $stt->bindValue(':type', $_FILES['photo']['type'], PDO::PARAM_STR);
+  $stt->bindValue(':data', $file, PDO::PARAM_LOB);
+  $stt->execute();
+  print '写真のアップロードに成功しました。';
+} catch(PDOException $e) {
+  print "エラーメッセージ:{$e->getMessage()}";
+}
