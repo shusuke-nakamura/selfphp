1bee75c62c87b30bde1675aea94252228fad439d
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-09-27 23:38:38 +0900

    P.447まで学習完了

---
+++ chap10/EliteBusinessPerson.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'BusinessPerson.php';
+
+class EliteBusinessPerson extends BusinessPerson {
+
+  public function work() {
+    print "<p>{$this->lastName}{$this->firstName}はバリバリ働いてます。</p>";
+  }
+}
---
+++ chap10/override3.php
@@ -0,0 +1,5 @@
+<?php
+require_once 'Foreigner.php';
+
+$hbp = new Foreigner('祥寛', 'ヨーダ', '山田');
+$hbp->show();
