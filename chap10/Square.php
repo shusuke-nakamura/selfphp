66a9aa1f0adb6544db0c0a1da72c6ab1ea019f3d
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-10-03 20:35:02 +0900

    P.457まで学習完了

--- chap10/Square.php
+++ chap10/Square.php
@@ -1,7 +1,24 @@
 <?php
-require_once 'Figure.php';
+// require_once 'Figure.php';
 
-class Square extends Figure {
+// class Square extends Figure {
+//   public function getArea(): float {
+//     return $this->width * $this->height;
+//   }
+// }
+
+require_once 'IFigure.php';
+
+class Square implements IFigure {
+
+  private $width;
+  private $height;
+
+  public function __construct(float $width, float $height) {
+    $this->width = $width;
+    $this->height = $height;
+  }
+
   public function getArea(): float {
     return $this->width * $this->height;
   }
--- chap10/Triangle.php
+++ chap10/Triangle.php
@@ -1,7 +1,24 @@
 <?php
-require_once 'Figure.php';
+// require_once 'Figure.php';
 
-class Triangle extends Figure {
+// class Triangle extends Figure {
+//   public function getArea(): float {
+//     return $this->width * $this->height / 2;
+//   }
+// }
+
+require_once 'IFigure.php';
+
+class Triangle implements IFigure {
+
+  private $width;
+  private $height;
+
+  public function __construct(float $width, float $height) {
+    $this->width = $width;
+    $this->height = $height;
+  }
+
   public function getArea(): float {
     return $this->width * $this->height / 2;
   }
