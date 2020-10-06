908087f27d1db1bdeea7994749cf4d6573ad8796
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-10-06 22:37:11 +0900

    P.485まで学習完了

---
+++ chap10/get_set.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'MyMail.php';
+
+$m = new MyMail();
+
+$m->to = 'shuchan.galaxy@gmail.com';
+$m->subject = 'テストメール';
+$m->message = 'こんにちは、MyMailクラスです。';
+
+$m->Form = 'admin@example.com';
+$m->X_Mailer = 'MyMail 1.0';
+$m->X_Priority = 1;
+$m->X_MSMail_Priority = 'High';
+
+$m->send();
