898d0f75b534b6f89d3d240d4ca4001f222dc74a
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2021-04-25 19:34:21 +0900

    P.60まで学習完了

---
+++ chap02/heredoc.php
@@ -0,0 +1,9 @@
+<?php
+$str = 'PHP(Hypertext Preprocessor)';
+$msg = <<<EOD
+{$str}は、サーバサイドで動作する簡易なスクリプト言語です。
+まずは、本書でじっくり基礎固めしましょう。<br />
+"Let's start, everyone!!"
+EOD;
+
+print $msg;
---
+++ chap02/string3.php
@@ -0,0 +1,7 @@
+<?php
+$title = 'サーバサイド技術の学び舎';
+$data1 = "サポートサイト\t「{$title}」へ<br />";
+$data2 = 'サポートサイト\t「{$title}」へ<br />';
+
+print $data1;
+print $data2;
