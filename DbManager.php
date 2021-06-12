4802cab4d6dd123a7bf047f2e86b6b2d7299c274
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2021-06-12 19:58:15 +0900

    P.396まで学習完了

--- DbManager.php
+++ DbManager.php
@@ -2,9 +2,10 @@
 function getDb()
 {
     $dsn = 'mysql:dbname=selfphp; host=127.0.0.1; charset=utf8';
-    $usr = 'selfphp';
+    $usr = 'selfusr';
     $passwd ='selfpass';
     // データベースへの接続を確立
     $db = new PDO($dsn, $usr, $passwd);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $db;
 }
