7cc4f911ed6bd7392c78e6b4df22f3ee05c92acd
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2021-06-27 20:56:33 +0900

    P.512まで学習完了

---
+++ chap10/full_qualifier.php
@@ -0,0 +1,5 @@
+<?php
+
+require 'wings/selfphp/chap10/MyClass.php';
+
+print \wings\selfphp\chap10\MyClass::showClass();
---
+++ chap10/use_scope.php
@@ -0,0 +1,6 @@
+<?php
+require_once './wings/selfphp/chap10/MyClass.php';
+
+function hoge() {
+    use wings\selfphp\chap10\MyClass;
+}
