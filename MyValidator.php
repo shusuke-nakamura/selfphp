4112919c559f19f52d6eb74ff4b23539ec4db629
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2021-06-30 23:29:34 +0900

    P.537の入力チェックの実装まで完了

--- MyValidator.php
+++ MyValidator.php
@@ -39,7 +39,78 @@ class MyValidator
     public function requiredCheck(string $value, string $name)
     {
         if (trim($value) === '') {
-            $this->_errors = "{$name}は必須入力です。";
+            $this->_errors[] = "{$name}は必須入力です。";
+        }
+    }
+
+    public function lengthCheck(string $value, string $name, int $len)
+    {
+        if (trim($value) !== '') {
+            if (mb_strlen($value) > $len) {
+                $this->_errors[] = "{$name}は{$len}文字以内で入力してください。";
+            }
+        }
+    }
+
+    public function rangeCheck(string $value, string $name, float $max, float $min)
+    {
+        if (trim($value) !== '') {
+            if ($value > $max || $value < $min) {
+                $this->_errors[] = "{$name}は{$min}～{$max}で指定してください。";
+            }
+        }
+    }
+
+    public function intTypeCheck(string $value, string $name)
+    {
+        if (trim($value) !== '') {
+            if (!ctype_digit($value)) {
+                $this->_errors[] = "{$name}は整数で入力してください。";
+            }
+        }
+    }
+
+    public function dateTypeCheck(string $value, string $name)
+    {
+        if (trim($value) !== '') {
+            $res = preg_split('|([/\-])|', $value);
+            if (count($res) !== 3 || !@checkdate($res[1], $res[2], $res[3])) {
+                $this->_errors[] = "{$name}は日付形式で入力してください。";
+            }
+        }
+    }
+
+    public function regexCheck(string $value, string $name, string $pattern)
+    {
+        if (trim($value) !== '') {
+            if (!preg_match($pattern, $value)) {
+                $this->_errors[] = "{$name}は正しい形式で入力してください。";
+            }
+        }
+    }
+
+    public function inArrayCheck(string $value, string $name, array $opts)
+    {
+        if (trim($value) !== '') {
+            if (!in_array($value, $opts)) {
+                $tmp = implode(',', $opts);
+                $this->_errors[] = "{$name}は{$tmp}の中から選択してください。";
+            }
+        }
+    }
+
+    public function duplicateCheck(string $value, string $name, string $sql)
+    {
+        try {
+            $db = getDb();
+            $stt = $db->prepare($sql);
+            $stt->bindValue(':value', $value);
+            $stt->execute();
+            if (($row = $stt->fetch()) !== false) {
+                $this->_errors[] = "{$name}は重複しています。";
+            }
+        } catch (PDOException $e) {
+            $this->_errors[] = $e->getMessage();
         }
     }
 
